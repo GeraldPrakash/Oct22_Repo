37dc5ec012fef648458001cab2ce86995fcd12f6
GeraldPrakash <getgerald@gmail.com>
2025-10-22 21:56:36 -0700

    Add files via upload

---
+++ Oct22_Repo1.py
@@ -0,0 +1,10 @@
+# create a repo and push this code into it
+# Download the code from repo 
+# modify the code
+# Upload the modified code into repo
+
+a = 10
+b = 20
+c = a+b
+print ("repo1 code")
+print ("Sum is :", c)
